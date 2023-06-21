fea62499e664adeda2ad8fc15f606512b6f4a8ee
smashyalts <47141527+smashyalts@users.noreply.github.com>
2023-06-21 02:56:12 +0200

    Initial Release

---
+++ src/main/java/net/craftsupport/noboost/NoBoost.java
@@ -0,0 +1,37 @@
+package net.craftsupport.noboost;
+
+import com.destroystokyo.paper.event.player.PlayerElytraBoostEvent;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.format.TextColor;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+
+public final class NoBoost extends JavaPlugin implements Listener {
+MiniMessage mm = MiniMessage.miniMessage();
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+        Bukkit.getServer().getPluginManager().registerEvents(this, this);
+saveDefaultConfig();
+    }
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+    @EventHandler
+    public void onBoost(PlayerElytraBoostEvent event) {
+        if (getConfig().getString("message") != null && getConfig().getBoolean("send-message")) {
+            Component message = mm.deserialize(getConfig().getString("message"));
+            event.getPlayer().sendMessage(message);
+            event.setCancelled(true);
+        }
+        event.setCancelled(true);
+    }
+}
